fc3f8ebf68c68ded1f883365fc620c70680251a3
Srinivas <kolipakasrinivas456@gmail.com>
2023-05-06 17:19:42 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Sidebar() {
+    return(
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button className="new-note" >+</button>
+            </div>
+            {/* {noteElements} */}
+        </section>
+    )
+}
+
+export default Sidebar
